bd2bac9116b614b16bbcd76c4692deda1eb4fce5
Saiful Habib <sangpejuang32@gmail.com>
2024-08-26 11:06:39 +0700

    perbaiki logic auth

--- sip_pdb/helper.py
+++ sip_pdb/helper.py
@@ -10,12 +10,16 @@ htmx = HTMX()
 def login_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
-        if 'logged_in' not in session or 'user_id' not in session or 'username' not in session:
+        if 'logged_in' not in session or 'is_admin' not in session:
+            return redirect(url_for('auth.login'))
+        elif session['logged_in'] == False:
+            session.clear()
+            return redirect(url_for('auth.login'))
+        elif 'user_id' not in session \
+            or 'username' not in session:
             # TODO: tambah pesan flash error
             # TODO: htmx disable hx-boost lewat server
-            return redirect(url_for('auth.login'))
-        elif session['logged_in'] == False or not isinstance(session['user_id'], int):
-            return redirect(url_for('auth.login'))
+            return redirect(url_for('admin.beranda'))
         return view(**kwargs)
     return wrapped_view
 
@@ -23,17 +27,11 @@ def admin_required(view):
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if 'is_admin' not in session or 'logged_in' not in session or 'admin_name' not in session:
-            return redirect(url_for('auth.admin_login'))
+            return redirect(url_for('auth.login_admin'))
         elif session['is_admin'] == False:
-            return redirect(url_for('auth.admin_login'))
+            session.clear()
+            return redirect(url_for('auth.login_admin'))
         return view(**kwargs)
     return wrapped_view
-
-# TODO: generate pdf!
-'''
-pake weasyprint
-css manual
-semua image dimasukkan pakai base64
-'''
 
 
