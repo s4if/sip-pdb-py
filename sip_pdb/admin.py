026672d1efea64e0c764e25ca038728ce30bfe95
Saiful Habib <sangpejuang32@gmail.com>
2024-08-26 09:51:05 +0700

    tambah lihat pendaftar

--- sip_pdb/admin.py
+++ sip_pdb/admin.py
@@ -27,21 +27,41 @@ route admin/back_to_admin/
 def lihat_pendaftar():
     return render_template('admin/lihat_pendaftar.jinja', is_htmx=htmx)
 
-'''
-example of multi join
-from sqlalchemy import aliased
-normal_order_alias = aliased(Order)
-black_friday_order_alias = aliased(Order)
-result = db.session.query(Customer).\
-    outerjoin(normal_order_alias, Customer.normal_order_id == normal_order_alias.id).\
-    outerjoin(black_friday_order_alias, Customer.black_friday_order_id == black_friday_order_alias.id).\
-    all()
-'''
+@bp.route('/data_pendaftar', methods=['GET'])
+@admin_required
+def data_pendaftar():
+    from .models import Registrant, Parent
+    from sqlalchemy.orm import aliased
+    father = aliased(Parent)
+    mother = aliased(Parent)
+    result = db.session.query(Registrant).\
+        outerjoin(father, Registrant.father_id == father.id).\
+        outerjoin(mother, Registrant.mother_id == mother.id).\
+        filter(Registrant.deleted == False).with_entities(
+            Registrant.id.label('id'), #0
+            Registrant.reg_id.label('reg_id'), #1
+            Registrant.name.label('name'), #2
+            Registrant.prev_school.label('prev_school'), #3
+            Registrant.selection_path.label('selection_path'), #4
+            Registrant.program.label('program'), #5
+            father.income.label('father_income'), #6
+            mother.income.label('mother_income'), #7
+        ).all()
+    data = []
+    for row in result:
+        item = []
+        item.append(row.id)
+        item.append(row.reg_id)
+        item.append(row.name)
+        item.append(row.prev_school)
+        item.append(row.selection_path)
+        item.append(row.program)
+        item.append(row.father_income)
+        item.append(row.mother_income)
+        item.append("""<button class="btn btn-sm btn-primary" hx-get="#">Edit</button>
+                    <button class="btn btn-sm btn-danger" hx-get="#">Delete</button>""")
+        data.append(item)
+        
+    return jsonify({'data':data})
 
-'''
-example of labeling in sqlalchemy
-result = db.session.query(Customer).outerjoin(Order, Customer.id == Order.customer_id).with_entities(
-    Customer.name.label('name'),
-    Order.name.label('item_name')
-).all()
-'''
+# TODO: Masuk dan keluar ke akun pendaftar!
