f4cc256f230767c535c40713d23d7b9c67e43822
Saiful Habib <sangpejuang32@gmail.com>
2024-08-29 02:37:53 +0700

    ganti password admin

--- sip_pdb/admin.py
+++ sip_pdb/admin.py
@@ -23,6 +23,27 @@ def beranda():
         is_htmx=htmx,
         **notif
 )
+    
+@bp.route('/ganti_password', methods=['GET', 'POST'])
+@admin_required
+def ganti_password():
+    from werkzeug.security import generate_password_hash, check_password_hash
+    if request.method == 'GET':
+        return render_template('admin/ganti_password.jinja', admin_name=session['admin_name'], is_htmx=htmx)
+    
+    if request.method == 'POST':
+        notif = {}
+        from .models import Admin
+        admin = Admin.query.filter_by(username=session['admin_name']).first()
+        if request.form['new_password'] != request.form['confirm_password']:
+            notif['error'] = 'Konfirmasi password tidak sesuai'
+        elif admin and check_password_hash(admin.password, request.form['current_password']):
+            admin.password = generate_password_hash(request.form['new_password'])
+            db.session.commit()
+            notif['success'] = 'Password berhasil diubah'
+        else:
+            notif['error'] = 'Password lama tidak sesuai'
+        return render_template('admin/ganti_password.jinja', admin_name=session['admin_name'], is_htmx=htmx, **notif)
 
 # TODO: Implement login sebagai pendaftar dengan memainkan isi session
 '''
